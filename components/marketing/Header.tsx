352556e5cc0a1fca42ad2a1caea03767513f5c77
Anyro <sirenxmedia@gmail.com>
2025-11-19 02:45:24 +1100

    fix: Increase Hero padding on mobile further, increase logo size on desktop

--- components/marketing/Header.tsx
+++ components/marketing/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
               alt="Seology.ai"
               width={120}
               height={32}
-              className="h-5 sm:h-6 md:h-7 lg:h-8 w-auto dark:invert"
+              className="h-5 sm:h-6 md:h-8 lg:h-10 w-auto dark:invert"
               priority
             />
           </Link>
